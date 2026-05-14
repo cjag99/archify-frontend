f431946757da8c8715b3bebfcd4bbb5cc588d030
Jose Enrique Aguilar <joseenriqueaguilar@Airdeadistrador.localdomain>
2026-05-14 09:46:54 +0200

    feat: install devicons library and refactor svg in footer

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,9 +7,6 @@ export default function DashboardPage() {
   return (
     <ProtectedRoute>
       <div className="min-h-screen grid grid-cols-12 gap-4 bg-linear-to-br from-slate-50 to-slate-100">
-        <div className="col-span-3">
-          <AdminNavBar />
-        </div>
         <div className="col-span-9">
           <h1 className="text-4xl font-bold mb-4">Welcome to your Dashboard, {user?.username}!</h1>
           <p className="text-lg text-gray-600">This is a protected page that only authenticated users can access.</p>
